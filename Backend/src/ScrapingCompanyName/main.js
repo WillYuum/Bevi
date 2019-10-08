2664bd610dfb9a0117197dc2d104ddf44fa9e182
William <liams_1998@hotmail.com>
2019-10-08 13:01:35 +0300

    added the root option

--- Backend/src/ScrapingCompanyName/main.js
+++ Backend/src/ScrapingCompanyName/main.js
@@ -1,12 +1,14 @@
 import loginToLInkined from "../public/loginFunc.js";
 
-
 /**
  * @async -run the scraping functionality
  */
 (async () => {
   try {
-    loginToLInkined("https://www.linkedin.com/login");
+    const root = await loginToLInkined("https://www.linkedin.com/login");
+    root.goto(
+      "https://www.linkedin.com/search/results/companies/?keywords=software%20company%2C%20lebanon&origin=SWITCH_SEARCH_VERTICAL&page=5"
+    );
   } catch (err) {
     console.log(err);
     throw new Error("Main functionality failed");
