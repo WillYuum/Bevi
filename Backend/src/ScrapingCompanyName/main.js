8d0ecf70be53b5742ed3c6759f9d568629fcc8b0
William <liams_1998@hotmail.com>
2019-10-11 11:17:06 +0300

    finished the scrape functionality for every page

--- Backend/src/ScrapingCompanyName/main.js
+++ Backend/src/ScrapingCompanyName/main.js
@@ -2,7 +2,9 @@ import loginToLInkinedin from "../publicFuncs/loginFunc.js";
 const url =
   "https://www.linkedin.com/search/results/companies/?keywords=software%20company%2C%20lebanon&origin=SWITCH_SEARCH_VERTICAL";
 
-let max_page = 0;
+//public variable to use in |recursiveScrappe| function
+let maxpage = 0;
+
 /**
  * @function Main The Main will run all all the fucntions to scrape all the companyNames
  */
@@ -10,20 +12,12 @@ const Main = async () => {
   try {
     const root = await loginToLInkinedin("https://www.linkedin.com/login");
     await root.goto(url);
-    let arr = [];
-    let startingPage = 1;
-    // max_page = await root.evaluate(() => {
-    //   const page = document.querySelector(
-    //     "li.artdeco-pagination__indicator:nth-last-Child(1)"
-    //   );
-    //   return parseInt(page.innerText);
-    // });
-    max_page = await getMaxPage(root);
-    console.log("max page", max_page);
-    const data = await ScrapeCompanyNames(root, url, startingPage, arr);
-    console.log("Here willy", data);
+    maxpage = await getMaxPage(root);
+    const x = await getNamesFromPage(root);
+    console.log(x);
+    // return recursiveScrappe(root, url, 1, maxpage, []);
   } catch (err) {
-    console.log(`Main scraping failed with = ${err}`);
+    console.log(`Fetching Main failed with = ${err}`);
   }
 };
 
@@ -34,52 +28,56 @@ const Main = async () => {
  */
 const getMaxPage = async root => {
   return await root.evaluate(() => {
-    const page = document.querySelector(
+    const maxPage = document.querySelector(
       "li.artdeco-pagination__indicator:nth-last-Child(1)"
     );
-    return parseInt(page.innerText);
+    return parseInt(maxPage.innerText);
   });
 };
 
-const getPageContent = async root => {
+/**
+ * @function getNamesFromPage - scrapes the names from the current page when called
+ * @param {class} root - root will provide you with browser and page functions
+ */
+const getNamesFromPage = async root => {
   return await root.evaluate(() => {
-    const page = [...document.querySelectorAll(
-      "h3.search-result__title"
-    )];
-    return page;
-  })
-}
+    const arr = [];
+    const names = document.querySelectorAll("h3.search-result__title");
+    console.log("names", names);
+    names.forEach(elem => {
+      arr.push(elem.innerText);
+    });
+    return arr;
+  });
+};
 
 /**
- * @function ScrapeCompanyNames this function is recusrivly scrape all the pages depending on the MaxPage
- * @param {class} root - root will provide you with browser and page functions
- * @param {object{url, int, array}} PageInfo - data information that has to be provided to scrape company names
+ * @function recursiveScrappe - it will scrape company names in every url and store them in array until current page === maxPage
+ * @param {class} root -root will provide you with browser and page functions
+ * @param {string} url - linkedin url
+ * @param {int} currentPage - the first number of the paginated page
+ * @param {int} max_page - the last number of the paginated page
+ * @param {array} list - this list will store all the company names
+ * @returns {array} - list of company names
  */
-async function ScrapeCompanyNames(root, url, currentPage, list = []) {
-  console.log("we are in he url ", url);
+const recursiveScrappe = async (root, url, currentPage, max_page, list) => {
   const newUrl = `${url}&page=${currentPage}`;
-  console.log("The new url is", newUrl);
   await root.goto(newUrl);
-  console.log("we are in page", await currentPage);
-  const names = await getPageContent(root);
- 
-  names.forEach(text => {
-    console.log(text.innerText);
-  })
+  const names = await getNamesFromPage(root);
 
   if (names) {
-    const newList = names.map(name => {
-      return name.innerText;
-    });
+    const newList = names;
     list = [...list, ...newList];
-  }
 
-  if (currentPage < max_page) {
-    ++currentPage;
-    return await ScrapeCompanyNames(root, url, currentPage, list);
+    if (currentPage <= max_page) {
+      ++currentPage;
+      return await recursiveScrappe(root, newUrl, currentPage, max_page, list);
+    } else {
+      console.log("The list of company names", list);
+    }
   } else {
-    return list;
+    console.log("There is something wrong with company names");
   }
-}
+};
 
 Main();
