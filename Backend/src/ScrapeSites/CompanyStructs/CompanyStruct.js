33f84f8d7e6b6b81ffffbf271e93a2efef13b2d8
William <liams_1998@hotmail.com>
2019-10-15 15:03:14 +0300

    fixing the saving file in main.js + fixing the company site struct

---
+++ Backend/src/ScrapeSites/CompanyStructs/CompanyStruct.js
@@ -0,0 +1,17 @@
+class CompanyStruct {
+  constructor(
+    CompanyLogo,
+    CompanyName,
+    CompanyLocationName,
+    CompanyType,
+    CompanyDescription,
+    CompanyWebLink
+  ) {
+    this.CompanyLogo = CompanyLogo;
+    this.CompanyName = CompanyName;
+    this.CompanyLocationName = CompanyLocationName;
+    this.CompanyType = CompanyType;
+    this.CompanyDescription = CompanyDescription;
+    this.CompanyWebLink = CompanyWebLink;
+  }
+}
