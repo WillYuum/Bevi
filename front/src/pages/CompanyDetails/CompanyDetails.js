ac22611ec7f395226e7b590595d729e3bda13be6
William <liams_1998@hotmail.com>
2019-10-20 21:34:01 +0300

    prepped up the pages component

---
+++ front/src/pages/CompanyDetails/CompanyDetails.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+//----------------IMPORT COMPONENTS------------------
+
+//----------------IMPORT COMPONENTS------------------
+
+import "./CompanyDetails.scss"
+
+class CompanyDetails extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className = "CompanyDetails-container">
+
+            </div>
+         );
+    }
+}
+ 
+export default CompanyDetails;
---
+++ front/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+//----------------IMPORT COMPONENTS------------------
+import HexMap from "../../map-component/HexMap/HexMap.js"
+import FilterMap from "../../map-component/FilterMap/FilterMap.js"
+//----------------IMPORT COMPONENTS------------------
+
+import "./LandingPage.scss";
+
+class LandingPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className = "LandingPage-container">
+
+            </div>
+         );
+    }
+}
+ 
+export default LandingPage;
